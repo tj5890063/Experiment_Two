2830821db6e97056b0a9ba274f4cc09cf5053964
Your Name <you@example.com>
2018-03-18 19:48:43 +0800

    第二次修改

--- app/src/main/java/com/example/administrator/hello_world1/MainActivity.java
+++ app/src/main/java/com/example/administrator/hello_world1/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.administrator.hello_world1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("msg","oncreate");
     }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("msg","onstart");
+    }
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.d("msg","onresume");
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("msg","onpause");
+        // Another activity is taking focus (this activity is about to be "paused").
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("msg","onstop");
+        // The activity is no longer visible (it is now "stopped")
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("msg","ondestroy");
+        // The activity is about to be destroyed.
+    }
+
 }
